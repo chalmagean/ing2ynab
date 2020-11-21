c7298761e3f3317bde84c3f9d15775e0114a06e5
Cezar Halmagean <cezar@mixandgo.com>
2020-11-21 08:56:13 +0200

    Updated test descriptions

--- test/ing_spec.rb
+++ test/ing_spec.rb
@@ -3,14 +3,14 @@ require "csv"
 
 describe Ing do
   describe ".csv" do
-    it "returns an empty list" do
+    it "returns an empty string" do
       CSV.open("testing.csv", "w") { |csv| csv << [] }
 
       assert_output("") { Ing.new("testing.csv").csv }
     end
 
     describe "when the file has transactions" do
-      it "returns a list of transactions" do
+      it "outputs data in CSV format" do
         CSV.open("testing.csv", "w") do |csv|
           csv << ["Date;Description;Credit;Debit"]
           csv << ["01 noiembrie 2020;Desc1;123.91"]
